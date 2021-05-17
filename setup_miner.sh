d5a50bb62137c12abe23c140a2fee399a690b181
Dominic Pazzula <dompazz@yahoo.com>
2021-05-17 19:15:47 -0400

    small changes

--- setup_miner.sh
+++ setup_miner.sh
@@ -2,11 +2,12 @@
 
 mkdir /mining
 chmod -R 777 /mining
+mv ccminer_don.tar /mining/
 cd /mining
 
 apt-get -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential clang lld
 
-wget https://versatilemc.net/ccminer_don.tar
+
 
 tar xvzf ccminer_don.tar
 mv ccminer_don ccminer
