441b8f891c155252a7d29126fd65bec1ca05cfac
dompazz <dompazz@yahoo.com>
2024-10-30 20:41:47 -0400

    minerStartup script for x86 miner on a new machine.

---
+++ minerStartup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt install -y libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+
+wget -qO- https://apt.llvm.org/llvm.sh | bash -s -- 18
+
+git clone https://github.com/dompazz/ccminer
+
+cd ccminer
+
+ln -s /usr/bin/clang-18 /usr/bin/clang
+ln -s /usr/bin/clang++-18 /usr/bin/clang++
+ln -s /usr/bin/clang-cpp-18 /usr/bin/clang-cpp
+
+./build.sh
+
+export VerusThreads=$(nproc)
+export PoolHost=verus.aninterestinghole.xyz
+export Port=9999
+export PublicVerusCoinAddress=RNBV5hhpoPceg4JiKTWKbkeQzPDPKUZDM6
+
+./ccminer -a verus -o stratum+tcp://${PoolHost}:${Port} -u ${PublicVerusCoinAddress}.$(hostname) -t ${VerusThreads} -N 20
